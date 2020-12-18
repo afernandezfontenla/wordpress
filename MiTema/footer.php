c8613b73984045dd62cd54ccbbc0369c764f6fcc
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2020-12-18 12:36:52 +0100

    Add files via upload

---
+++ MiTema/footer.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1> Google Chrome </h1>"; ?>
+<img src="<?php echo get_stylesheet_directory_uri();?>/Imagen1.jpg">
+<h3>Historia</h3>
+<p>El navegador fue presentado en público de manera oficial el 2 de septiembre de 2008 para Microsoft Windows (únicamente XP y versiones posteriores) en 43 idiomas, siendo una versión beta. Chrome obtuvo rápidamente cerca del 1% del mercado de navegadores.</p>
+<h1>Miguel de Cervantes</h1>
+<img src="<?php echo get_stylesheet_directory_uri();?>/Miguel2.jpg">
+<h3>Obras</h3>
+<p>La Galatea, El ingenioso hidalgo don Quijote de la Mancha, Novelas ejemplares, El ingenioso caballero don Quijote de la Mancha, Los trabajos de Persiles y Sigismunda</p>
